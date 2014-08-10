238af8be41f6aba7dc9cf3a084d8f707a5b33eea
Chad A. Carino <chadc@tekserve.com>
2014-08-10 14:07:11 -0400

    parse link for menu item

--- js/menu-item-function.js
+++ js/menu-item-function.js
@@ -6,12 +6,27 @@ jQuery( document ).ready(function( $ ) {
 		$ajaxMenuItems.each(function() {
 			$(this).find('a').click(function( event ) {
 				event.preventDefault();
-				loadContent( $(this).parent().attr('class') );
+				loadContent( $(this).attr('href') );
 				
 			});
 		});
 	}
-	function loadContent( contentSlug ) {
-		console.log( contentSlug );
+	function loadContent( href ) {
+		console.log( 'href: ' + href );
+		var slug = getSlug( href );
+		console.log( 'slug: ' + slug );
 	}
 });
+function getSlug(url)
+{
+	   var qstart = url.indexOf('?');
+       var query = qstart >= 0 ? url.substring(qstart + 1) : null;
+       if( query ) {
+		   var vars = query.split("&");
+		   for (var i=0;i<vars.length;i++) {
+				   var pair = vars[i].split("=");
+				   if(pair[0] == 'ajaxmenucontent'){return pair[1];}
+		   }
+		}
+       return(false);
+}
